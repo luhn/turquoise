7f1c28f375585561d4e7535151fcc6048a5c1d28
Theron Luhn <theron@luhn.com>
2016-03-29 16:54:56 -0700

    More metadata.

--- setup.py
+++ setup.py
@@ -1,10 +1,27 @@
 from setuptools import setup, find_packages
 
+
 setup(
     name='turquoise',
     version='0.1.0',
+    description=(
+        'A utility for performing blue-green deployments on AWS Auto Scaling '
+        + 'Groups'
+    ),
+    author='Theron Luhn',
+    author_email='theron@luhn.com',
+    url='https://github.com/luhn/turquoise',
     packages=find_packages(),
     entry_points={
         'console_scripts': ['turquoise=turquoise:main'],
     },
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+    ],
 )
